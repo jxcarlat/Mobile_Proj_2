a87e59dfe557d5ff2f0c0b13a41c1e79d49e62bc
jxcarlat <35707407+jxcarlat@users.noreply.github.com>
2018-10-09 17:24:51 -0700

    Push to desktop

--- magic_8_ball/app/src/main/java/com/example/josh/magic_8_ball/MainActivity.kt
+++ magic_8_ball/app/src/main/java/com/example/josh/magic_8_ball/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.josh.magic_8_ball
 
+import android.content.Context
 import android.hardware.Sensor
 import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -11,7 +13,7 @@ import android.widget.TextView
 import java.util.Random
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
 
     var myStrings: Array<String> = arrayOf(
             "It is certain", "You may rely on it",
@@ -25,6 +27,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mySensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        myAccelerometer = mySensorManager!!.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
         fun ClosedRange<Int>.random() =
                 Random().nextInt((endInclusive + 1) - start) + start
         var indexPoint: Int = (0..9).random()
@@ -40,12 +44,26 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+    override fun onResume(){
+        super.onResume()
+
+        mySensorManager!!.registerListener(this, myAccelerometer, SensorManager.SENSOR_DELAY_UI)
+    }
+
+    override fun onPause(){
+        super.onPause()
+
+        mySensorManager!!.unregisterListener(this)
+    }
     override fun onSensorChanged(event: SensorEvent?) {
         // This is the function that will be updated every time our
         // accelerometer sends an update to our app
-        Log.d("UALR_UTIL", "X Reading: " + event!!.values[0].toString())
-        Log.d("UALR_UTIL", "Y Reading: " + event!!.values[1].toString())
-        Log.d("UALR_UTIL", "Z Reading: " + event!!.values[2].toString())
+        Log.d("EXAMPLE_UTIL", "X Reading: " + event!!.values[0].toString())
+        Log.d("EXAMPLE_UTIL", "Y Reading: " + event!!.values[1].toString())
+        Log.d("EXAMPLE_UTIL", "Z Reading: " + event!!.values[2].toString())
     }
 
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+
+    }
 }
